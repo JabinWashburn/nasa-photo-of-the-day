64a7367c9497f94389a65a215ccf0b7c54ddad42
JabinWashburn <jabinwashburn@gmail.com>
2020-03-18 15:52:32 -0700

    working on setting up components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import axios from 'axios';
+import './Content'; 
+
 
 const App = () => {
-  const [photo, setPhoto] = useState([]);
-  
-  useEffect(() => {
-    axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then (response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log('There is an error jabin', error)
-    });
-  });
 
   return <div className='App'>
 
